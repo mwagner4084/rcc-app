8341ed96c41d229cc9d1ef498fde186f7a6f9a2f
Marissa Wagner <mconnell4084@mail.kvcc.edu>
2023-11-07 12:25:42 -0500

    add content to services template add mobile responsiveness to css files

--- pages/services.tsx
+++ pages/services.tsx
@@ -1,29 +1,83 @@
-// pages/index.js
-import Head from 'next/head';
+// pages/services.tsx
 import Image from 'next/image';
 import styles from '../styles/Services.module.css';
 
-export default function Home() {
+export default function AboutPage() {
   return (
     <div className={styles.body}>
-        <div className={styles.container}>
-            <main className={styles.main}>
-                <div className={styles.hero}>
-                    <div className={styles.section}>
-                        <Image
-                        className={styles.logo}
-                        src="/img/logo-transparent.png"
-                        alt="RSC Logo"
-                        width={400}
-                        height={300}
-                        priority />
-                    </div>
-                    <h1 className={styles.title}>RSC Construction Services, LLC.</h1>
-                    <p className={styles.subtitle}>Your trusted partner in Handyman and Carpentry Services.</p>
-                    <button className={styles.ctaButton}>Get a quote</button>
+        <main className={styles.main}>
+            <h1 className={styles.title}>
+                Services Offered
+            </h1>
+            <hr></hr>
+            <div className={styles.grid}>
+                <div className={styles.card}>
+                    <h2>
+                        Handyman Services &rarr;
+                    </h2>
+                    <p>
+                        General home repairs, maintenance, and small-scale projects.
+                    </p>
                 </div>
-            </main>
-        </div>
+                <div className={styles.card}>
+                    <h2>
+                        Carpentry &rarr;
+                    </h2>
+                    <p>
+                        Custom-built furniture, cabinetry, and structural carpentry services.
+                    </p>
+                </div>
+                <div className={styles.card}>
+                    <h2>
+                        Drywall Repair &rarr;
+                    </h2>
+                    <p>
+                        Filling, patching, and texturing to keep your walls in perfect condition.
+                    </p>
+                </div>
+                <div className={styles.card}>
+                    <h2>
+                        Masonry Repointing &rarr;
+                    </h2>
+                    <p>
+                        Rejuvenating your stonework for both aesthetics and structural integrity.
+                    </p>
+                </div>
+                <div className={styles.card}>
+                    <h2>
+                        Deck Repair &rarr;
+                    </h2>
+                    <p>
+                        Structural repairs and refinishing to extend the life of your outdoor space.
+                    </p>
+                </div>
+                <div className={styles.card}>
+                    <h2>
+                        Home Improvement &rarr;
+                    </h2>
+                    <p>
+                        Remodeling, additions, and other home improvement projects.
+                    </p>
+                </div>
+                <div className={styles.card}>
+                    <h2>
+                        Weather Proofing
+                    </h2>
+                    <p>
+                        Sealing gaps, insulating spaces, and other services to keep you comfortable year-round.
+                    </p>
+                </div>
+                <div className={styles.card}>
+                    <h2>
+                        Lite Plumbing and Electrical
+                    </h2>
+                    <p>
+                        Fixing leaks, rewiring, and general plumbing and electrical maintenance.
+                    </p>
+                </div>
+                <button className={styles.ctaButton}>Request a quote</button>
+            </div>
+        </main>
     </div>
   );
 }
