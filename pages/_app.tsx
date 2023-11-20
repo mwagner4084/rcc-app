ae5697b4a06618dbaf7068beaa5633234d45cd83
Marissa Wagner <marissawagner@Marissas-MacBook-Pro.local>
2023-11-20 12:44:54 -0500

    add google font scripts back to __app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,14 +8,14 @@ import Head from "next/head"
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className="flex flex-col min-h-screen">
-      {/* <Head>
+      <Head>
         <link rel="stylesheet" href="/styles.globals.css"></link>
         <link rel="preconnect" href="https://fonts.gstatic.com"></link>
         <link rel="preconnect" href="https://fonts.googleapis.com"></link>
         <link href="https://fonts.googleapis.com/css?family=Mohave:wght@400;600&display=swap" rel="stylesheet"></link>
         <link href="https://fonts.googleapis.com/css?family=Major+Birch:wght@400;600&display=swap" rel="stylesheet"></link>
         <link href="https://fonts.googleapis.com/css?family=Vina+Sans:wght@400;600&display=swap" rel="stylesheet"></link>
-      </Head> */}
+      </Head>
       <Navbar />
       <div className="flex-grow">
         <Component {...pageProps} />
